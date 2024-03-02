24f5bc96342cdc9013d9528ff1068d5ac18a0224
Maykon Mendel <maykonmendel.dev@gmail.com>
2024-03-02 15:31:04 -0300

    feat: Adicionando novas migrations
    Adicionando novas migrations e atualizando o banco de dados

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,7 +1,13 @@
-﻿namespace BlazorShop.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorShop.Models;
 
 public class Category
 {
+    [Key]
     public int Id { get; set; }
+    [Required(ErrorMessage = "Informe o título")]
+    [MinLength(3, ErrorMessage = "A categoria deve ter pelo menos 3 caracteres")]
+    [MaxLength(60, ErrorMessage = "A categoria deve ter pelo no máximo 60 caracteres")]
     public string Title { get; set; } = string.Empty;
 }
