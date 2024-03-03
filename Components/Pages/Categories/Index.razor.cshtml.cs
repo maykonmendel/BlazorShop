7a0b4b1844b4175727b2c7e04f422320bc8e6fe5
Maykon Mendel <maykonmendel.dev@gmail.com>
2024-03-03 19:14:01 -0300

    feat: Crud de Categorias
    Crud de categorias seguindo a abordagem de uma página para cada ação - Create/Read/Update/Delete

---
+++ Components/Pages/Categories/Index.razor.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorShop.Components.Pages.Categories;
+
+public class Index_razor : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
